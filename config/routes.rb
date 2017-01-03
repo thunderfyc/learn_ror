9311d7fe60e6efbdffceed45cbe4ce43b3dd376b
Yicheng Fan <thunderfyc@gmail.com>
2017-01-02 18:55:01 -0800

    Add Devise user and sign in with Linkedin

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # get 'welcome/index'
   root 'welcome#index'
 
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
